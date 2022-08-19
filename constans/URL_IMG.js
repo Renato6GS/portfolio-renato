6e52348710250d52b769695a37d94667d820a2b3
Renatogranados <renatogranadosgt@gmail.com>
2022-08-18 23:51:51 -0600

    feat: add header desktop and change idiom

---
+++ constans/URL_IMG.js
@@ -0,0 +1,6 @@
+export const URL_IMG = {
+  RENATO: '/img/renato-avatar.jpg',
+  MIDUDEV: '/img/midudev-avatar.png',
+  CRISTINA: '/img/midudev-cristina.png',
+  BERNARDO: '/img/midudev-bernardo.png',
+};
